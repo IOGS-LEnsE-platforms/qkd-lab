9d3ad0af0fd1d7b94140b364e0e7a2ed57baa3f0
noamchoppinet <noam.choppinet@outlook.fr>
2025-06-25 16:50:55 +0200

    Update process_model.py

--- qkd_lab_app/models/process_model.py
+++ qkd_lab_app/models/process_model.py
@@ -11,3 +11,20 @@ class processModel():
     def __init__(self):
         self.cpc = AureaCPC()
         self.htdc = AureaHTDC()
+    
+    def filter_noise(self, delays):
+        '''extracts the spike in the histogram (list of delays) and returns its delay value
+        and the new histogram'''
+        delays_cor = np.array(delays.copy())
+        delays_cor[delays_cor<100]
+        index_dict = {}
+        for delay in delays_cor:
+            if not delay in index_dict.keys():
+                index_dict[delay] = 1
+            else:
+                index_dict[delay] += 1
+        delay_maximum = max(index_dict.values())
+        for delay in index_dict.keys():
+            if index_dict[delay] == delay_maximum:
+                return delay, [delay for i in range(delay_maximum)]
+        return None, None
